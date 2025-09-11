7a83b0ffa91c6226d4aaaead21a1100a5708926a
Daniel <danielalexbrogio@gmail.com>
2025-09-11 16:55:57 -0300

    Aula Geral

---
+++ Zuera/Nova pasta/lol.py
@@ -0,0 +1,31 @@
+import pyautogui as py
+import time as t
+import os 
+
+py.press('win')
+
+py.write('chrome')
+t.sleep(1)
+py.press('enter')
+t.sleep(1)
+py.press('F11')
+t.sleep(1)
+py.click(857,435)
+t.sleep(1)
+py.write('Galos de Combate')
+t.sleep(1)
+py.press('enter')
+t.sleep(1)
+py.click(298,183)
+t.sleep(1)
+py.click(469,362)
+t.sleep(1)
+py.scroll(-500)
+t.sleep(1)
+py.click(783,559)
+t.sleep(1)
+py.rightClick(1537,471)
+
+
+
+
